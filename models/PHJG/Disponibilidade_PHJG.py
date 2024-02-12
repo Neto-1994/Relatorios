b3d51fc6bfea7c35d19930f0106cec0640e06208
Neto-1994 <tiredes503@gmail.com>
2024-02-12 11:15:19 -0300

    Criado arquivo Validacao..

--- models/PHJG/Disponibilidade_PHJG.py
+++ models/PHJG/Disponibilidade_PHJG.py
@@ -1,11 +1,7 @@
-import os.path
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from Conexao import obter_conexao
-
+from Validacao import validar
 
 class Disp_PHJG():
     def main(self, Planilha, data1, data2, mes, ano):
@@ -29,30 +25,8 @@ class Disp_PHJG():
             return valores
 
         def registrar_valores(valores):
-            # Parte de log da API Google Sheets
-            # If modifying these scopes, delete the file token.json.
-            SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
-            creds = None
-            # The file token.json stores the user's access and refresh tokens, and is
-            # created automatically when the authorization flow completes for the first
-            # time.
-            if os.path.exists("E:/Relatorios/token.json"):
-                creds = Credentials.from_authorized_user_file(
-                    "E:/Relatorios/token.json", SCOPES)
-            # If there are no (valid) credentials available, let the user log in.
-            if not creds or not creds.valid:
-                if creds and creds.expired and creds.refresh_token:
-                    creds.refresh(Request())
-                else:
-                    flow = InstalledAppFlow.from_client_secrets_file(
-                        "E:/Relatorios/CredenciaisJair.json", SCOPES
-                    )
-                    creds = flow.run_local_server(port=0)
-                # Save the credentials for the next run
-                with open("E:/Relatorios/token.json", "w") as token:
-                    token.write(creds.to_json())
-
             try:
+                creds = validar()
                 service = build("sheets", "v4", credentials=creds)
                 if mes == 1:
                     # Atualizar nomes das abas
